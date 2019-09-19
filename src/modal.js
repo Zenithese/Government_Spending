4e21096f8989ead31445074fe4dbdf8eeb6440e9
Justin <jandersen770@gmail.com>
2019-09-18 20:08:09 -0400

    Add DOMContentLoaded for background video

--- src/modal.js
+++ src/modal.js
@@ -1,4 +1,5 @@
-document.getElementById("modal-container").innerHTML = `<button id="myBtn">FREEDOM SPENDING</button>
+document.getElementById("modal-container").innerHTML = `<div class="header-left">FREEDOM SPENDING</div><div class="header-right"><button id="myBtn">ABOUT</button></div>
+
  
                                                     <div id="myModal" class="modal">
 
@@ -8,8 +9,8 @@ document.getElementById("modal-container").innerHTML = `<button id="myBtn">FREED
                                                                 <h2>Freedom Spending</h2>
                                                             </div>
                                                             <div class="modal-body">
-                                                                <p>Digital Accountability and Transparency Act of 2014 (DATA Act) (P.L. 113-101)</p>
-                                                                <p>Types of spending</p>
+                                                                <p>This data vizualization was made possible via the Digital Accountability and Transparency Act of 2014 (DATA Act) (P.L. 113-101) signed by President Obama in May 2014. Thanks to the data's accessibilty I was able to create a visualization of Federal spending parsed into the US Treasury's budget functions. Additonally, here you'll find federal spending per state. I hope this project insipres increasingly incisive data visualizations that increase transperency.</p>
+                                                                <p>In the spirit of transparency, consider the differnent types of federal spending, mainly: discresionary and mandatory. Discresionary spendings are decided via a appropreation process by congress every year, whereas mandatory do not require that same yearly process. Addtionally, tax breaks are considered tax expenditures (i.e. it sacrifices tax revenue to the benefit of specific economic sectors). These considerations would be a healthy addition for this project, and are being considered for future development.</p>
                                                             </div>
                                                             <div class="modal-footer">
                                                                 <h3>
